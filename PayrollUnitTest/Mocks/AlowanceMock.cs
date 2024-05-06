5b574503dffac8b3d5028b42848785cc180ec6d2
Chathuranga Dinesh <chathuranga.ruh.lk@gmail.com>
2024-05-07 02:06:31 +0530

    added unit test

---
+++ PayrollUnitTest/Mocks/AlowanceMock.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using Payroll.Domain.Dtos;
+using Payroll.Domain.Entities;
+using Payroll.Domain.Repositories;
+
+namespace PayrollUnitTest.Mocks
+{
+    public class AlowanceMock
+    {
+        static Random rand = new Random();       
+
+        public static List<AlowanceDto> alowances = new List<AlowanceDto>()
+        {
+            new AlowanceDto()
+            {
+                EmployeeId = rand.Next(),
+                DepartmentId = rand.Next(),
+                Date = DateTime.Now,
+                Amount = (float)rand.NextDouble(),
+                Status = "Pending"
+            },
+            new AlowanceDto()
+            {
+                EmployeeId = rand.Next(),
+                DepartmentId = rand.Next(),
+                Date = DateTime.Now,
+                Amount = (float)rand.NextDouble(),
+                Status = "Completed"
+            }
+        };
+
+        public static IAlowanceRepository GetAlowanceRepository()
+        {
+            var moq = new Mock<IAlowanceRepository>();
+            moq.Setup(x => x.GetAlowanceListAsync()).Returns(Task.FromResult(alowances));
+            return moq.Object;
+        }
+    }
+}
---
+++ PayrollUnitTest/Tests/AlowanceTest.cs
@@ -0,0 +1,31 @@
+﻿using Payroll.Application.Handlers;
+using Payroll.Application.Queries;
+using Payroll.Domain.Repositories;
+using PayrollUnitTest.Mocks;
+
+namespace PayrollUnitTest.Tests
+{
+    public class AlowanceTest
+    {
+        private IAlowanceRepository _repository;
+        private GetAlowanceListHandler _handler;
+
+        public AlowanceTest()
+        {
+            _repository = AlowanceMock.GetAlowanceRepository();
+            _handler = new GetAlowanceListHandler(_repository);
+        }
+
+        [Fact]
+        public async Task GetAlowanceListAsync()
+        {
+            //Assign
+            var request = new GetAlowanceListQuery();
+            var token = new CancellationToken();
+            //Act
+            var result = await _handler.Handle(request, token);
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
