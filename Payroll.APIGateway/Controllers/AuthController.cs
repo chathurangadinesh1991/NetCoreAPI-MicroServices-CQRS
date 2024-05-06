a3dc0486e3f18799b34409be650f7d46b442e848
Chathuranga Dinesh <chathuranga.ruh.lk@gmail.com>
2024-05-07 01:30:32 +0530

    api gateway changes

---
+++ Payroll.APIGateway/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Payroll.APIGateway.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Login([FromBody] object user)
+        {
+            bool isSucessfull = true;
+            try
+            {
+                // Login logics
+                // Token genaration
+
+
+                if (isSucessfull)
+                    return Ok(); // Success results with token
+
+                return Unauthorized();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
